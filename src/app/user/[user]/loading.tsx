d6b646282416286620258be5a30cc0d606b0519e
unknown <amaechinaikechukwu6@gmail.com>
2023-09-06 12:26:05 +0100

    deleted a folder

--- src/app/user/[user]/loading.tsx
+++
@@ -1,7 +0,0 @@
-
-import { usePathname } from 'next/navigation'
-export default function Loading() {
-  
-  // Or a custom loading skeleton component
-  return <p className='font-semibold text-gray-500'>Fetching user</p>
-}
