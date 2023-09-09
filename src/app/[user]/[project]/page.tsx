a0f425abd251e1bcaa59e34da731d229ba7744ff
unknown <amaechinaikechukwu6@gmail.com>
2023-09-09 09:55:06 +0100

    fix project load error

--- src/app/[user]/[project]/page.tsx
+++ src/app/[user]/[project]/page.tsx
@@ -57,7 +57,7 @@ const {user,project } = params;
   const result = await getData(project.toLowerCase());
   const {projectData} =result 
     const {userData}= await getUser(user)
-   const result = await AddClicks(userData.uid,projectData.nickname)
+   const resultclicks = await AddClicks(userData.uid,projectData.nickname)
   // optionally access and extend (rather than replace) parent metadata
  
   return {
