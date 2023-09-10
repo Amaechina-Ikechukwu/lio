1a77659e6cd78a8ff9935e5de95e9bf04f93c9d9
unknown <amaechinaikechukwu6@gmail.com>
2023-09-10 20:44:22 +0100

    rebuild

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,29 @@
-import Image from 'next/image'
-import   HeroBody  from '@/components/Landing/HeroBody'
+import Image from "next/image";
+import HeroBody from "@/components/Landing/HeroBody";
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "lio",
+  description: "Easy platform of compile your portfolio",
+  openGraph: {
+    title: "lio",
+    description: "Easy platform of compile your portfolio",
+    images: [
+      "https://firebasestorage.googleapis.com/v0/b/lio-6af30.appspot.com/o/images%2FXRcZM4rs7DaSCStCCIvgkuVrlxh2%2FFrame%209.png?alt=media&token=f8e11522-c3c3-4c09-917b-353e837c90e5",
+    ],
+  },
+  icons: {
+    icon: "https://firebasestorage.googleapis.com/v0/b/lio-6af30.appspot.com/o/images%2FXRcZM4rs7DaSCStCCIvgkuVrlxh2%2FFrame%209.png?alt=media&token=f8e11522-c3c3-4c09-917b-353e837c90e5",
+  },
+};
 export default function Page() {
   return (
     <main className=" h-screen">
-
-    <div className="h-full ">
-  <div className=" h-full">
- 
-    <HeroBody/>
-  </div>
-</div>
-
+      <div className="h-full ">
+        <div className=" h-full">
+          <HeroBody />
+        </div>
+      </div>
     </main>
-  )
+  );
 }
