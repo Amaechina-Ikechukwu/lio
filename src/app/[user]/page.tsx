9558610ea666772b806069bb8eb2978709030128
unknown <amaechinaikechukwu6@gmail.com>
2023-09-11 15:29:59 +0100

    tiny adjustments

--- src/app/[user]/page.tsx
+++ src/app/[user]/page.tsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 import lio from "../../../../public/lio.png";
 import { EnvelopeIcon } from "@heroicons/react/24/solid";
 import UserProjects from "../../components/Profile/UserProjects";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram } from '@fortawesome/free-solid-svg-icons'
+import Link from 'next/link';
 async function getData(user: string) {
   const username = user.toLowerCase();
   const res = await fetch(
@@ -86,6 +89,31 @@ export default async function Page({ params }: { params: { user: string } }) {
   const result = await getData(params.user);
   const projects = await getProjects(params.user);
   const { userData } = result;
+const socials = [
+  {
+    social: "github",
+    icon: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+  },
+  {
+    social: "twitter",
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/512px-Logo_of_Twitter.svg.png"
+  },
+  {
+    social: "linkedin",
+    icon: "https://cdn-icons-png.flaticon.com/512/174/174857.png"
+  },
+  {
+    social: "instagram",
+    icon: "https://cdn.sanity.io/images/kts928pd/production/0debd6925122aa7cd6c1bf9f3bc2e5a948314c9a-1024x1024.png"
+  },
+  {
+    social: "facebook",
+    icon: "https://img.freepik.com/premium-vector/blue-social-media-logo_197792-1759.jpg"  },
+  {
+    social: "tiktok",
+    icon:"https://img.freepik.com/premium-vector/tik-tok-logo_578229-290.jpg"
+  }
+];
 
   return (
     <div className=" space-y-28  ">
@@ -95,12 +123,34 @@ export default async function Page({ params }: { params: { user: string } }) {
             {userData.displayName}
           </h2>
           <p className="leading-7">{userData.bio}</p>
-          <button className=" py-2 flex space-x-2 transition duration-300 transform hover:scale-105 ">
-            <h3 className=" underline-offset-1 font-light font-open-sans">
-              {userData.email}
-            </h3>
-            <EnvelopeIcon className="h-6 w-6 text-gray-500" />
-          </button>
+          <a
+  className="py-2 flex space-x-2 transition duration-300 transform hover:scale-105"
+  href={`mailto:${userData.email}`}
+>
+  <h3 className="underline-offset-1 font-light font-open-sans">
+    {userData.email}
+  </h3>
+  <EnvelopeIcon className="h-6 w-6 text-gray-500" />
+</a>
+<div className='flex space-x-4 drop-shadow-md'>
+  {socials.map((soc) =>
+    userData[soc.social] && userData[soc.social].length > 0 && (
+      <button
+        className="py-2 flex space-x-2 transition duration-300 transform hover:scale-150"
+        key={soc.social}
+      >
+        <Link href={userData[soc.social]}>
+         <Image height={20} width={20} src={soc.icon} alt={`${userData.displayName} ${soc.social}`} className='opacity-50' />
+        </Link>
+      </button>
+    )
+  )}
+</div>
+<div>
+
+
+</div>
+
         </div>
         <Image
           src={userData.photoURL}
