26626060ca29902fa23b0a3ebf2fa4c3ffb85f19
unknown <amaechinaikechukwu6@gmail.com>
2023-09-11 15:29:59 +0100

    tiny adjustments

--- next.config.js
+++ next.config.js
@@ -15,6 +15,49 @@ const nextConfig = {
         port: '',
         // pathname: '/account123/**',
       },
+      {
+        protocol: 'https',
+        hostname: "www.flaticon.com",
+        port: '',
+        // pathname: '/account123/**',
+      },
+      {
+        protocol: 'https',
+        hostname: "fontawesome.com",
+        port: '',
+        // pathname: '/account123/**',
+      },
+        {
+        protocol: 'https',
+        hostname: "github.githubassets.com",
+        port: '',
+        // pathname: '/account123/**',
+      },
+        {
+        protocol: 'https',
+        hostname: "upload.wikimedia.org",
+        port: '',
+        // pathname: '/account123/**',
+      },
+        {
+        protocol: 'https',
+        hostname: "cdn-icons-png.flaticon.com",
+        port: '',
+        // pathname: '/account123/**',
+      },
+
+        {
+        protocol: 'https',
+        hostname: "img.freepik.com",
+        port: '',
+        // pathname: '/account123/**',
+      },
+      {
+        protocol: 'https',
+        hostname: "cdn.sanity.io",
+        port: '',
+        // pathname: '/account123/**',
+      },
     ],
   },
 }
