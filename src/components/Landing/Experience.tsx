482fd860fcf7527ed52bdf8cfe4bccb5c52402c6
unknown <amaechinaikechukwu6@gmail.com>
2023-09-30 10:44:43 +0100

    hero description rewording and adding app link

--- src/components/Landing/Experience.tsx
+++ src/components/Landing/Experience.tsx
@@ -1,54 +1,79 @@
-import LioHome from "../lio-web.png"
-import LioWebUser from "../lio-web-user.png"
-import LioWebProject from "../lio-web-project.png"
-import LioH from "../lio-home.jpg"
-import LioInput from "../lio-input.jpg"
-import LioProfile from "../lio-profile.jpg"
+import LioHome from "../lio-web.png";
+import LioWebUser from "../lio-web-user.png";
+import LioWebProject from "../lio-web-project.png";
+import LioH from "../lio-home.jpg";
+import LioInput from "../lio-input.jpg";
+import LioProfile from "../lio-profile.jpg";
 
-import Image from 'next/image'
-export default function Experience(){
-	const data=[
-{
-	title:'For Up Coming Developers',
-	use:"Lio provides a platform for upcoming developers to share even the most little or basic of projects until they have their portfolio site",
-	image:LioWebUser
-},
-{
-	title:"Dont Hide That Project",
-	use:`Add as many projects as you want, it doesn't need to be that good, it shows you are learning everyday`,
-	image:LioWebProject
-},{
-	title:'Discoverability',
-	use:'Visitor can search and discover developers with ease, exposing thousand of projects to the world',
-	image:LioHome,
-}
-		]
-	return(
-		<div className='space-y-24 w-full'>
-			{data.map((data,index)=>(
-<div key={data.title} className='flex flex-col md:flex-row justify-between items-center'>
-	<div className="md:w-3/6 space-y-6" >
-		<h2 className="text-4xl text-gray-700 sm:leading-loose  text-start font-bold">
-			{data.title}
-		</h2>
-		<p className="text-xl font-regular text-start text-gray-600 leading-lg">
-			{data.use}
-		</p>
-	</div>
-	<Image src={data.image} width={300} alt={data.title}  className="w-full sm:w-auto aspect-square object-contain  rounded-md w-[500px] md:w-[500px]"/>
-</div>
-				))}
-			<div className='flex flex-col md:flex-row items-center justify-between space-y-6'>
-				<button className="bg-gray-900 w-full md:w-3/6   text-gray-200 rounded-xl shadow-xl py-4 px-4 font-bold text-xl transition duration-300 transform hover:scale-105 hover:bg-opacity-80 focus:outline-none ring ring-light-accent">
+import Image from "next/image";
+export default function Experience() {
+  const data = [
+    {
+      title: "For Up Coming Developers",
+      use: "Lio provides a platform for upcoming developers to share even the most little or basic of projects until they have their portfolio site",
+      image: LioWebUser,
+    },
+    {
+      title: "Dont Hide That Project",
+      use: `Add as many projects as you want, it doesn't need to be that good, it shows you are learning everyday`,
+      image: LioWebProject,
+    },
+    {
+      title: "Discoverability",
+      use: "Visitor can search and discover developers with ease, exposing thousand of projects to the world",
+      image: LioHome,
+    },
+  ];
+  return (
+    <div className="space-y-24 w-full">
+      {data.map((data, index) => (
+        <div
+          key={data.title}
+          className="flex flex-col md:flex-row justify-between items-center"
+        >
+          <div className="md:w-3/6 space-y-6">
+            <h2 className="text-4xl text-gray-700 sm:leading-loose  text-start font-bold">
+              {data.title}
+            </h2>
+            <p className="text-xl font-regular text-start text-gray-600 leading-lg">
+              {data.use}
+            </p>
+          </div>
+          <Image
+            src={data.image}
+            width={300}
+            alt={data.title}
+            className="w-full sm:w-auto aspect-square object-contain  rounded-md w-[500px] md:w-[500px]"
+          />
+        </div>
+      ))}
+      <div className="flex flex-col md:flex-row items-center justify-between space-y-6">
+        <button className="bg-gray-900 w-full md:w-3/6   text-gray-200 rounded-xl shadow-xl py-4 px-4 font-bold text-xl transition duration-300 transform hover:scale-105 hover:bg-opacity-80 focus:outline-none ring ring-light-accent">
+          <a href="https://expo.dev/artifacts/eas/vHTyT34NvDbrXDjiNcvpTS.apk">
             Get Started With Lio App
-          </button> 
-      <div className='md:grid md:grid-cols-2 space-y-6 md:gap-14 justify-center'>
-  <Image src={LioH} width={300} alt={"lio homescreen"} className="w-full sm:w-auto aspect-square object-contain rounded-md w-[500px] md:w-[200px]" />
-  <Image src={LioProfile} width={300} alt={"Lio Profile"} className="w-full sm:w-auto aspect-square object-contain rounded-md w-[500px] md:w-[200px]" />
-  <Image src={LioInput} width={300} alt={'lio-input'} className="w-full sm:w-auto aspect-square object-contain rounded-md w-[500px] md:w-[200px] justify-self-center" />
-</div>
-
-         
-			</div>
-		</div>)
+          </a>
+        </button>
+        <div className="md:grid md:grid-cols-2 space-y-6 md:gap-14 justify-center">
+          <Image
+            src={LioH}
+            width={300}
+            alt={"lio homescreen"}
+            className="w-full sm:w-auto aspect-square object-contain rounded-md w-[500px] md:w-[200px]"
+          />
+          <Image
+            src={LioProfile}
+            width={300}
+            alt={"Lio Profile"}
+            className="w-full sm:w-auto aspect-square object-contain rounded-md w-[500px] md:w-[200px]"
+          />
+          <Image
+            src={LioInput}
+            width={300}
+            alt={"lio-input"}
+            className="w-full sm:w-auto aspect-square object-contain rounded-md w-[500px] md:w-[200px] justify-self-center"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Landing/HeroBody.tsx
+++ src/components/Landing/HeroBody.tsx
@@ -1,8 +1,8 @@
-"use client"
-import React from 'react';
-import Link from 'next/link'
-import {useEffect,useState,useRef} from 'react'
-import SearchModal from '@/components/Constants/SearchModal'
+"use client";
+import React from "react";
+import Link from "next/link";
+import { useEffect, useState, useRef } from "react";
+import SearchModal from "@/components/Constants/SearchModal";
 
 export default function HeroBody() {
   const [openModal, setOpenModal] = useState<any>();
@@ -13,27 +13,38 @@ export default function HeroBody() {
         {/* Hero Text */}
         <div className="mb-10 space-y-6 flex flex-col items-start justify-center h-full ">
           <h1 className="text-5xl text-gray-900 sm:leading-loose  text-start font-bold">
-            Let the world see all you{' '}
-            <span className="p-3 text-white rounded-full bg-light-accent mt-10">do</span>
+            Let the world see all you{" "}
+            <span className="p-3 text-white rounded-full bg-light-accent mt-10">
+              do
+            </span>
           </h1>
           <p className="text-xl font-regular text-start text-gray-700 leading-lg">
-            Every project holds immense potential, waiting for its moment to shine.
-            You possess the ingenuity, and now it&apos;s time to showcase your accomplishments
-            on a platform that amplifies your{' '}
-            <span className="text-light-alterna">pride</span> and dedication.
-            Let the world witness the extraordinary impact of your work – because no
-            endeavor is trivial when it&apos;s a testament to your{' '}
-            <span className="text-light-alterna">passion and creativity</span>.
+            No portfolio site yet?. Lio provides a platform to display your
+            projects. &apos; In addition to displaying your projects, Lio
+            provides tools to track your progress and measure the impact of your
+            work. Analyze performance metrics, see which projects are getting
+            the most attention, and use these insights to refine your portfolio
+            and improve your professional presence. Don't wait any longer to
+            share your talent with the world.
           </p>
-<div className='flex flex-col w-full sm:w-fit items-start sm:flex-row space-y-4 sm:space-y-0 space-x-0 sm:space-x-8'><button className="bg-white w-full sm:w-fit  text-black rounded-xl shadow-xl py-4 px-4 font-regular transition duration-300 transform hover:scale-105 hover:bg-opacity-80 focus:outline-none ring ring-light-accent">
-            Download Lio App
-          </button>
-          <button onClick={() => setOpenModal('initial-focus')} className="bg-gray-900 w-full sm:w-fit text-gray-200 rounded-xl shadow-xl py-4 px-4 font-bold transition duration-300 transform hover:scale-105  hover:ring-gray-200 focus:outline-none ring ring-light-accent">
-          
-            Search For A Portfolio
-          </button></div>
-           {/*<Button onClick={() => setOpenModal('initial-focus')}>Toggle modal</Button>*/}
-           <SearchModal openModal={openModal} setOpenModal={()=>setOpenModal(undefined)} />
+          <div className="flex flex-col w-full sm:w-fit items-start sm:flex-row space-y-4 sm:space-y-0 space-x-0 sm:space-x-8">
+            <button className="bg-white w-full sm:w-fit  text-black rounded-xl shadow-xl py-4 px-4 font-regular transition duration-300 transform hover:scale-105 hover:bg-opacity-80 focus:outline-none ring ring-light-accent">
+              <a href="https://expo.dev/artifacts/eas/vHTyT34NvDbrXDjiNcvpTS.apk">
+                Download Lio App
+              </a>
+            </button>
+            <button
+              onClick={() => setOpenModal("initial-focus")}
+              className="bg-gray-900 w-full sm:w-fit text-gray-200 rounded-xl shadow-xl py-4 px-4 font-bold transition duration-300 transform hover:scale-105  hover:ring-gray-200 focus:outline-none ring ring-light-accent"
+            >
+              Search For A Portfolio
+            </button>
+          </div>
+          {/*<Button onClick={() => setOpenModal('initial-focus')}>Toggle modal</Button>*/}
+          <SearchModal
+            openModal={openModal}
+            setOpenModal={() => setOpenModal(undefined)}
+          />
         </div>
       </div>
     </div>
